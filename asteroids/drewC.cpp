774a5b2c52af268e068fffbaf2e773d28bf30dbd
Drew Cody <dcody@cs.csubak.edu>
2015-05-18 21:45:58 -0700

    CMD line arguements, ship invulverable and better weapon when in supermode, added accuracy guage, added win/lose message

--- asteroids/drewC.cpp
+++ asteroids/drewC.cpp
@@ -3,100 +3,101 @@
 //Assignment: HW3
 
 #include <cmath>
+#include "structures.h"
 extern "C" {
 #include "fonts.h"
 }
-#include "structures.h"
+extern const int super;
 
 void resizeAsteroid(Asteroid *a)
 {
-    if (a->radius < 50 && a->radius > 25) {
-	a->nverts = 4;
-	a->radius = rnd()*80.0 + 40.0;
-	Flt r2 = a->radius / 2.0;
-	Flt angle = 0.0f;
-	Flt inc = (PI * 2.0) / (Flt)a->nverts;
-	for (int i=0; i<a->nverts; i++) {
-	    a->vert[i][0] = sin(angle) * (r2 + rnd() * a->radius);
-	    a->vert[i][1] = cos(angle) * (r2 + rnd() * a->radius);
-	    angle += inc;
-	}
-	a->color[0] = 1;
-	a->color[1] = 0.0;
-	a->color[2] = 0.0;
-    } 
+	if (a->radius < 50 && a->radius >= 21) {
+		a->nverts = 4;
+		a->radius = rnd()*80.0 + 40.0;
+		Flt r2 = a->radius / 2.0;
+		Flt angle = 0.0f;
+		Flt inc = (PI * 2.0) / (Flt)a->nverts;
+		for (int i=0; i<a->nverts; i++) {
+			a->vert[i][0] = sin(angle) * (r2 + rnd() * a->radius);
+			a->vert[i][1] = cos(angle) * (r2 + rnd() * a->radius);
+			angle += inc;
+		}
+		a->color[0] = 1.0;
+		a->color[1] = 1.0;
+		a->color[2] = 1.0;
+		a->color[3] = 1.0;
+	} 
 }
 
 float bulletAccuracy(Game *g)
 {
-    int hit, fired;
-    float accuracy;
-    hit = g->bulletsHit;
-    fired = g->bulletsFired;
-    accuracy = (hit/fired) * 100;
-    return accuracy;
+	float accuracy;
+	accuracy = (g->bulletsHit/g->bulletsFired) * 100;
+	return accuracy;
 }
 
 void asteroidCollision(Asteroid *a, Game *g)
 {
-    while (a) {
-	float d0, d1, dist, shipVelocity, asteroidVelocity;
-	d0 = g->ship.pos[0] - a->pos[0];
-	d1 = g->ship.pos[1] - a->pos[1];
-	dist = (d0*d0 + d1*d1);
-	if (dist < (a->radius * a->radius) * 0.5) {
-	    //Calculate damage taken from impact with asteroid
-	    shipVelocity = abs(g->ship.vel[0] + g->ship.vel[1]);
-	    asteroidVelocity = abs(a->vel[0] + a->vel[1]);
+	while (a) {
+		float d0, d1, dist, shipVelocity, asteroidVelocity;
+		d0 = g->ship.pos[0] - a->pos[0];
+		d1 = g->ship.pos[1] - a->pos[1];
+		dist = (d0*d0 + d1*d1);
+		if (dist < (a->radius * a->radius) * 0.5) {
+			//Calculate damage taken from impact with asteroid
+			shipVelocity = abs(g->ship.vel[0] + g->ship.vel[1]);
+			asteroidVelocity = abs(a->vel[0] + a->vel[1]);
 
-	    if ( g->ship.superMode >= 50 ) {
-		g->ship.damageTaken += 0;
-	    }
-	    else if (shipVelocity >= 2.5 || asteroidVelocity >= 2.5) {
-		g->ship.damageTaken += 5;
-	    }
-	    else if (shipVelocity >= 1.5 || asteroidVelocity >= 1.5) {
-		g->ship.damageTaken += 2;
-	    } else {
-		g->ship.damageTaken++;
-	    }
-	    //Alter ships position and velocity	
-	    g->ship.pos[0] = a->pos[0] + d0 * 1.5;
-	    g->ship.pos[1] = a->pos[1] + d1 * 1.5;
-	    g->ship.vel[0] *= -0.5f;
-	    g->ship.vel[1] *= -0.5f;
-	    a->vel[0] += g->ship.vel[0] * -0.5;
-	    a->vel[1] += g->ship.vel[1] * -0.5;
+			if ( g->ship.superMode >= super ) {
+				g->ship.damageTaken += 0;
+			}
+			else if (shipVelocity >= 2.5 || asteroidVelocity >= 2.5) {
+				g->ship.damageTaken += 5;
+			}
+			else if (shipVelocity >= 1.5 || asteroidVelocity >= 1.5) {
+				g->ship.damageTaken += 2;
+			} else {
+				g->ship.damageTaken++;
+			}
+			//Alter ships position and velocity	
+			g->ship.pos[0] = a->pos[0] + d0 * 1.5;
+			g->ship.pos[1] = a->pos[1] + d1 * 1.5;
+			g->ship.vel[0] *= -0.25f;
+			g->ship.vel[1] *= -0.25f;
+			//Alter asteroid velocity
+			a->vel[0] += g->ship.vel[0] * -0.25;
+			a->vel[1] += g->ship.vel[1] * -0.25;
+		}
+		a = a->next;
 	}
-	a = a->next;
-    }
 }
 
 bool endGame(Game *g)
 {
-    if (g->ship.damageTaken >= 100 || g->nasteroids == 0) {
-	return true;
-    } else {
-	return false;
-    }
+	if (g->ship.damageTaken >= 100 || g->nasteroids == 0) {
+		return true;
+	} else {
+		return false;
+	}
 }
+
 void endMenu(Game *g)
 {
-    int yellow = 0x00ffff00;
-    int red = 0x00ff0000;
-    int hit, fired;
-    hit = g->bulletsHit;
-    fired = g->bulletsFired;
-    float accuracy = (hit / fired) * 100;
-    Rect r;
-    r.bot = yres - 300;
-    r.left = 600;
-    ggprint16(&r, 50, red, "GAME OVER");
-    ggprint16(&r, 50, yellow, "Your Score: %i", g->score);
-    ggprint16(&r, 50, yellow, "Your Time: %i seconds",g->gameTimer);
-    ggprint16(&r, 50, yellow, "Your Accuracy: %i",accuracy);
-    r.bot = yres - 600;
-    //ggprint16(&r, 50, red, "Press R to Restart");
-    ggprint16(&r, 50, yellow, "Press ESC to Exit");
+	int yellow = 0x00ffff00;
+	int red = 0x00ff0000;
+	Rect r;
+	r.bot = yres - 300;
+	r.left = 600;
+	ggprint16(&r, 50, red, "GAME OVER");
+	if (g->ship.damageTaken >= 100) {
+		ggprint16(&r, 50, red, "YOUR SHIP WAS DESTROYED");
+	} else {
+		ggprint16(&r, 50, red, "YOU WIN!");
+	}
+	ggprint16(&r, 50, yellow, "Your Score: %i", g->score);
+	ggprint16(&r, 50, yellow, "Your Time: %i seconds",g->gameTimer);
+	ggprint16(&r, 50, yellow, "Your Accuracy: %4.2f%",g->accuracy);
+	r.bot = yres - 600;
+	ggprint16(&r, 50, yellow, "Press ESC to Exit");
 }
 
--- asteroids/structures.h
+++ asteroids/structures.h
@@ -7,8 +7,8 @@
 #define VecCopy(a,b) (b)[0]=(a)[0];(b)[1]=(a)[1];(b)[2]=(a)[2]
 #define VecDot(a,b)     ((a)[0]*(b)[0]+(a)[1]*(b)[1]+(a)[2]*(b)[2])
 #define VecSub(a,b,c) (c)[0]=(a)[0]-(b)[0]; \
-		      (c)[1]=(a)[1]-(b)[1]; \
-    		      (c)[2]=(a)[2]-(b)[2]
+							 (c)[1]=(a)[1]-(b)[1]; \
+(c)[2]=(a)[2]-(b)[2]
 
 #define PI 3.141592653589793
 #define ALPHA 1
@@ -21,85 +21,88 @@ extern int yres;
 
 
 struct Ship {
-    Vec dir;
-    Vec pos;
-    Vec vel;
-    float angle;
-    float color[3];
-    int superMode;
-    struct timespec superTimer;
-    int damageTaken;
-    Ship() {
-	VecZero(dir);
-	pos[0] = (Flt)(xres/2);
-	pos[1] = (Flt)(yres/2);
-	pos[2] = 0.0f;
-	VecZero(vel);
-	angle = 0.0;
-	color[0] = 1.0;
-	color[1] = 1.0;
-	color[2] = 1.0;
-	superMode = 0;
-	damageTaken = 0;
-    }
+	Vec dir;
+	Vec pos;
+	Vec vel;
+	float angle;
+	float color[3];
+	int superMode;
+	struct timespec superTimer;
+	int damageTaken;
+	Ship() {
+		VecZero(dir);
+		pos[0] = (Flt)(xres/2);
+		pos[1] = (Flt)(yres/2);
+		pos[2] = 0.0f;
+		VecZero(vel);
+		angle = 0.0;
+		color[0] = 1.0;
+		color[1] = 1.0;
+		color[2] = 1.0;
+		superMode = 0;
+		damageTaken = 0;
+	}
 };
 
 struct Bullet {
-    Vec pos;
-    Vec vel;
-    float color[3];
-    struct timespec time;
-    struct Bullet *prev;
-    struct Bullet *next;
-    Bullet() {
-	prev = NULL;
-	next = NULL;
-    }
+	Vec pos;
+	Vec vel;
+	float color[3];
+	struct timespec time;
+	struct Bullet *prev;
+	struct Bullet *next;
+	Bullet() {
+		prev = NULL;
+		next = NULL;
+		color[0] = 1.0f;
+		color[1] = 1.0f;
+		color[2] = 1.0f;
+	}
 };
 
 struct Asteroid {
-    Vec pos;
-    Vec vel;
-    int nverts;
-    Flt radius;
-    Vec vert[4];
-    float angle;
-    float rotate;
-    float color[3];
-    struct Asteroid *prev;
-    struct Asteroid *next;
-    Asteroid() {
-	prev = NULL;
-	next = NULL;
-    }
+	Vec pos;
+	Vec vel;
+	int nverts;
+	Flt radius;
+	Vec vert[4];
+	float angle;
+	float rotate;
+	float color[3];
+	struct Asteroid *prev;
+	struct Asteroid *next;
+	Asteroid() {
+		prev = NULL;
+		next = NULL;
+	}
 };
 
 struct Game {
-    Ship ship;
-    Asteroid *ahead;
-    Bullet *bhead;
-    int nasteroids;
-    int nbullets;
-    int gameTimer;
-    int bulletsFired;
-    int bulletsHit;
-    float accuracy;
-    bool gameOver;
-    unsigned long int score;
-    struct timespec bulletTimer;
-    struct timespec asteroidTimer;
-    Game() {
-	ahead = NULL;
-	bhead = NULL;
-	nasteroids = 0;
-	nbullets = 0;
-	gameTimer = 0;
-	score = 0;
-	gameOver = false;
-	bulletsFired = 0;
-	bulletsHit = 0;
-	accuracy = 0.0;
-    }
+	Ship ship;
+	Asteroid *ahead;
+	Bullet *bhead;
+	int nasteroids;
+	int nbullets;
+	int gameTimer;
+	float bulletsFired;
+	float bulletsHit;
+	float accuracy;
+	bool gameOver;
+	unsigned long int score;
+	struct timespec bulletTimer;
+	struct timespec asteroidTimer;
+	Game() {
+		ahead = NULL;
+		bhead = NULL;
+		nasteroids = 0;
+		nbullets = 0;
+		gameTimer = 0;
+		score = 0;
+		gameOver = false;
+		bulletsFired = 0.0;
+		bulletsHit = 0.0;
+		accuracy = 0.0;
+	}
 };
 
 #endif
